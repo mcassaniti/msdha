9513092336f5d81d86cf1df8379b3b35c50b1ac5
Michael Cassaniti <michael@cassaniti.id.au>
2018-11-19 17:47:18 +1100

    frontend: Better handling of frontend stop and start

--- frontend/run.sh
+++ frontend/run.sh
@@ -60,7 +60,7 @@ node_change_detect_loop() {
         [ "$line" = "master" -a "$(cat "$MSDHA_STATE_DIR/current_master")" != "$node" ] && update_master "$node"
 
         # Master removed
-        [ -z "$line" -a "$(cat "$MSDHA_STATE_DIR/current_master")" = "$node" ] && update_master "$MSDHA_MASTER_DEFAULT"
+        [ -z "$line" -a "$(cat "$MSDHA_STATE_DIR/current_master")" = "$node" ] && update_master
 
         ;;
     esac
@@ -75,10 +75,15 @@ update_master() {
   sed s/'BACKEND_PORT'/"$MSDHA_PORT"/ \
   > /haproxy.cfg
 
-  echo "MSDHA: Backend now $1"
   echo -n "$1" > "$MSDHA_STATE_DIR/current_master"
   [ -f /run/haproxy.pid ] && killall haproxy > /dev/null
-  haproxy -f /haproxy.cfg -p /run/haproxy.pid
+  if [ -z $1 ] ; then
+    echo "MSDHA: Backend removed and proxy stopped"
+    rm -f /run/haproxy.pid
+  else
+    haproxy -f /haproxy.cfg -p /run/haproxy.pid
+    echo "MSDHA: Backend now $1"
+  fi
 }
 
 ### Initialization ###
